9fb0f4b842864fdfbf9af52be2578d4ff1f6e421
Shubham Goel <shubham.goel@gmail.com>
2015-05-17 02:24:07 -0700

    every thing prettyfied

--- js/modules/movieTable.js
+++ js/modules/movieTable.js
@@ -1,151 +1,151 @@
 var mt = function() {
-    var exports = {};
-    var margin = {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-        },
-        width = 180 - margin.left - margin.right,
-        rowWidth = 25,
-        height = 675 - margin.top - margin.bottom;
+  var exports = {};
+  var margin = {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    },
+    width = 180 - margin.left - margin.right,
+    rowWidth = 25,
+    height = 675 - margin.top - margin.bottom;
 
-    var x = d3.scale.linear()
-        .range([0, width])
-        .domain([0, 10]);
+  var x = d3.scale.linear()
+    .range([0, width])
+    .domain([0, 10]);
 
-    var y = d3.scale.linear();
+  var y = d3.scale.linear();
 
-    var svg = d3.select(".lca-movie-table").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select(".lca-movie-table").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.json("data/movies/movies_1990.json", function(error, data) {
-        y.range([0, rowWidth * data.length])
-            .domain([0, data.length]);
+  d3.json("data/movies/movies_1990.json", function(error, data) {
+    y.range([0, rowWidth * data.length])
+      .domain([0, data.length]);
 
-        svg.selectAll(".lca-movie")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "lca-movie")
-            .attr("x", 0)
-            .attr("y", function(data, index) {
-                return y(index);
-            })
-            .attr('width', function(data) {
-                if (x(data.shotCount) > width) {
-                    return width;
-                } else {
-                    return x(data.shotCount);
-                }
-            })
-            .attr('height', rowWidth - 1)
+    svg.selectAll(".lca-movie")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "lca-movie")
+      .attr("x", 0)
+      .attr("y", function(data, index) {
+        return y(index);
+      })
+      .attr('width', function(data) {
+        if (x(data.shotCount) > width) {
+          return width;
+        } else {
+          return x(data.shotCount);
+        }
+      })
+      .attr('height', rowWidth - 1)
 
-        .on('mouseover.hideotherspots', function(d, i) {
-                filter('movieTableHover', d, i);
-            })
-            .on('mouseout.showallspots', function(d, i) {
-                filter('movieTableHoverEnd', '', '');
-            });
-        // 	.on('mouseover.dst', dstSelected)
-        // //	.on('mouseout', tip_circle.hide)
-        // 	.on('mousedown', sourceSelected);
+    .on('mouseover.hideotherspots', function(d, i) {
+        filter('movieTableHover', d, i);
+      })
+      .on('mouseout.showallspots', function(d, i) {
+        filter('movieTableHoverEnd', '', '');
+      });
+    // 	.on('mouseover.dst', dstSelected)
+    // //	.on('mouseout', tip_circle.hide)
+    // 	.on('mousedown', sourceSelected);
 
 
-        svg.selectAll(".lca-movie-label")
-            .data(data)
-            .enter().append('text')
-            .attr('class', 'lca-movie-label')
-            .attr('x', 0)
-            .attr('y', function(data, index) {
-                return y(index) - 10;
-            })
-            .attr('dy', '.16em')
-            .text(function(data) {
-                return data.title
-            });
+    svg.selectAll(".lca-movie-label")
+      .data(data)
+      .enter().append('text')
+      .attr('class', 'lca-movie-label')
+      .attr('x', 0)
+      .attr('y', function(data, index) {
+        return y(index) - 10;
+      })
+      .attr('dy', '.16em')
+      .text(function(data) {
+        return data.title
+      });
 
-    });
+  });
 
-    exports.reloadMovieWithTimePeriod = function(timePeriod) {
-        var duration = 200;
-        var timePeriodFile = 'data/movies/movies_' + timePeriod + '.json';
-        console.log('reloadMovieWithTimePeriod: ' + timePeriodFile);
+  exports.reloadMovieWithTimePeriod = function(timePeriod) {
+    var duration = 200;
+    var timePeriodFile = 'data/movies/movies_' + timePeriod + '.json';
+    console.log('reloadMovieWithTimePeriod: ' + timePeriodFile);
+
+    svg.selectAll(".lca-movie, .lca-movie-label")
+      .transition()
+      .duration(duration)
+      .ease("quad")
+      .style('opacity', 0);
+
+
+    setTimeout(function() {
+      d3.json(timePeriodFile, function(error, data) {
+        console.log('reloadMovieWithTimePeriod: data loaded');
+
+        var movieRow = svg.selectAll(".lca-movie")
+          .data(data);
+
+        movieRow.enter().append("rect")
+          .attr("class", "lca-movie");
+
+        movieRow
+          .attr("x", 0)
+          .attr("y", function(data, index) {
+            return y(index);
+          })
+          .attr('width', function(data) {
+            if (x(data.shotCount) > width) {
+              return width;
+            } else {
+              return x(data.shotCount);
+            }
+          })
+          .attr('height', rowWidth - 1)
+          .on('mouseover.hideotherspots', function(d, i) {
+            filter('movieTableHover', d, i);
+          })
+          .on('mouseout.showallspots', function(d, i) {
+            filter('movieTableHoverEnd', '', '');
+          });;
+
+        var movieLabel = svg.selectAll('.lca-movie-label')
+          .data(data);
+
+        movieLabel.enter().append('text')
+          .attr('class', 'lca-movie-label')
+
+        movieLabel
+          .attr('x', 0)
+          .attr('y', function(data, index) {
+            return y(index) - 10;
+          })
+          .attr('dy', '.16em')
+          .text(function(data) {
+            return data.title
+          })
+          .on('mouseover.hideotherspots', function(d, i) {
+            filter('movieTableHover', d, i);
+          })
+          .on('mouseout.showallspots', function(d, i) {
+            filter('movieTableHoverEnd', '', '');
+          });;
+
 
         svg.selectAll(".lca-movie, .lca-movie-label")
-            .transition()
-            .duration(duration)
-            .ease("quad")
-            .style('opacity', 0);
+          .transition()
+          .duration(duration)
+          .ease("quad")
+          .style('opacity', 1);
 
-
-        setTimeout(function() {
-            d3.json(timePeriodFile, function(error, data) {
-                console.log('reloadMovieWithTimePeriod: data loaded');
-
-                var movieRow = svg.selectAll(".lca-movie")
-                    .data(data);
-
-                movieRow.enter().append("rect")
-                    .attr("class", "lca-movie");
-
-                movieRow
-                    .attr("x", 0)
-                    .attr("y", function(data, index) {
-                        return y(index);
-                    })
-                    .attr('width', function(data) {
-                        if (x(data.shotCount) > width) {
-                            return width;
-                        } else {
-                            return x(data.shotCount);
-                        }
-                    })
-                    .attr('height', rowWidth - 1)
-                    .on('mouseover.hideotherspots', function(d, i) {
-                        filter('movieTableHover', d, i);
-                    })
-                    .on('mouseout.showallspots', function(d, i) {
-                        filter('movieTableHoverEnd', '', '');
-                    });;
-
-                var movieLabel = svg.selectAll('.lca-movie-label')
-                    .data(data);
-
-                movieLabel.enter().append('text')
-                    .attr('class', 'lca-movie-label')
-
-                movieLabel
-                    .attr('x', 0)
-                    .attr('y', function(data, index) {
-                        return y(index) - 10;
-                    })
-                    .attr('dy', '.16em')
-                    .text(function(data) {
-                        return data.title
-                    })
-                    .on('mouseover.hideotherspots', function(d, i) {
-                        filter('movieTableHover', d, i);
-                    })
-                    .on('mouseout.showallspots', function(d, i) {
-                        filter('movieTableHoverEnd', '', '');
-                    });;
-
-
-                svg.selectAll(".lca-movie, .lca-movie-label")
-                    .transition()
-                    .duration(duration)
-                    .ease("quad")
-                    .style('opacity', 1);
-
-                movieLabel.exit().remove();
-                movieRow.exit().remove();
-            });
-        }, duration);
-    };
-    return exports;
+        movieLabel.exit().remove();
+        movieRow.exit().remove();
+      });
+    }, duration);
+  };
+  return exports;
 }();
 
 
--- js/modules/shotmap.js
+++ js/modules/shotmap.js
@@ -1,200 +1,199 @@
 // spot map
 var sm = function() {
-    var exports = {};
-    var margin = {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-        },
-        width = 675 - margin.left - margin.right,
-        height = 675 - margin.top - margin.bottom;
+  var exports = {};
+  var margin = {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    },
+    width = 675 - margin.left - margin.right,
+    height = 675 - margin.top - margin.bottom;
 
-    var mapBounds = {
-        top: 37.84,
-        right: -122.35,
-        bottom: 37.68,
-        left: -122.55
-    };
+  var mapBounds = {
+    top: 37.84,
+    right: -122.35,
+    bottom: 37.68,
+    left: -122.55
+  };
 
-    var x = d3.scale.linear()
-        .range([0, width])
-        .domain([mapBounds.left, mapBounds.right]);
+  var x = d3.scale.linear()
+    .range([0, width])
+    .domain([mapBounds.left, mapBounds.right]);
 
-    var y = d3.scale.linear()
-        .range([height, 0])
-        .domain([mapBounds.bottom, mapBounds.top]);
+  var y = d3.scale.linear()
+    .range([height, 0])
+    .domain([mapBounds.bottom, mapBounds.top]);
 
-    var color = d3.scale.category10();
+  var color = d3.scale.category10();
 
-    var tip = d3.tip()
-        .attr('class', 'lca-spot-tip-tap')
-        .offset([19, 4])
-        .html(function(d) {
-            return "<div class='lca-spot-tip-head'></div> <div class='lca-spot-tip'><span>"
-            + d.title + 
-            "</span></div>";
-        })
-        .direction('e');
+  var tip = d3.tip()
+    .attr('class', 'lca-spot-tip-tap')
+    .offset([19, 4])
+    .html(function(d) {
+      return "<div class='lca-spot-tip-head'></div> <div class='lca-spot-tip'><span>" + d.title +
+        "</span></div>";
+    })
+    .direction('e');
 
-    var svg = d3.select(".lca-spot-map").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select(".lca-spot-map").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.call(tip);
+  svg.call(tip);
 
-    // todo: change this loader
-    d3.json("data/time_1990.json", function(error, data) {
-        // console.log(data);
-        svg.selectAll(".lca-spot")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "lca-spot")
-            // .attr("r", 3.5)
-            .attr("x", function(d) {
-                if (d.Longitude) {
-                    return x(d.Longitude);
-                } else {
-                    return 0
-                }
-            })
-            .attr("y", function(d) {
-                if (d.Latitude) {
-                    return y(d.Latitude);
-                } else {
-                    return 0
-                }
-                // return y(d.Latitude); 
-            })
-            .attr('width', 10)
-            .attr('height', 10);
-        // .style("fill", 'red' );
-    });
-
-    exports.reloadMapWithTimePeriod = function(timePeriod) {
-        var duration = 200;
-        var timePeriodFile = 'data/map/map_' + timePeriod + '.json';
-        console.log('reloadMapWithTimePeriod: ' + timePeriodFile);
-        svg.selectAll(".lca-spot")
-            .transition()
-            .duration(duration)
-            .ease("quad")
-            .style('opacity', 0);
-
-        setTimeout(function() {
-            d3.json(timePeriodFile, function(error, data) {
-                var spotSquares = svg.selectAll(".lca-spot")
-                    .data(data);
-
-                spotSquares.enter().append("rect")
-                    .attr("class", "lca-spot");
-
-                spotSquares
-                    .attr("x", function(d) {
-                        if (d.long) {
-                            return x(d.long);
-                        } else {
-                            return 0
-                        }
-                    })
-                    .attr("y", function(d) {
-                        if (d.lat) {
-                            return y(d.lat);
-                        } else {
-                            return 0
-                        }
-                        // return y(d.Latitude); 
-                    })
-                    .attr('width', 10)
-                    .attr('height', 10)
-                    .on('mouseover', tip.show)
-                    .on('mouseout', tip.hide)
-                    .on('mousedown', function(d, i) {
-                        filter('spotSelected', d, i);
-                    });
-
-                spotSquares
-                    .transition()
-                    .duration(duration)
-                    .ease("quad")
-                    .style('opacity', 1);
-
-                spotSquares.exit().remove();
-            });
-        }, duration);
-    };
-
-    exports.highlightSpotsForMovie = function(movieID) {
-
-        if (movieID == null) {
-            svg.selectAll(".lca-spot")
-                .style('stroke', '#FFCD00');
+  // todo: change this loader
+  d3.json("data/time_1990.json", function(error, data) {
+    // console.log(data);
+    svg.selectAll(".lca-spot")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "lca-spot")
+      // .attr("r", 3.5)
+      .attr("x", function(d) {
+        if (d.Longitude) {
+          return x(d.Longitude);
         } else {
-            svg.selectAll(".lca-spot")
-                .style('stroke', function(data) {
-                    if (movieID == data.movieid) {
-                        return '#FFCD00';
-                    } else {
-                        return '#646464';
-                    }
-                });
-        };
+          return 0
+        }
+      })
+      .attr("y", function(d) {
+        if (d.Latitude) {
+          return y(d.Latitude);
+        } else {
+          return 0
+        }
+        // return y(d.Latitude); 
+      })
+      .attr('width', 10)
+      .attr('height', 10);
+    // .style("fill", 'red' );
+  });
 
-        // .style("stroke", function(data) {
-        //     if (data.destination == dest) {
-        //         return "#619EB5";
-        //     } else {
-        //         return "#C8C8C8";
-        //     }
-        // })
-        // .style("stroke-width", function(data) {
-        //     if (data.destination == dest) {
-        //         return 2;
-        //     } else {
-        //         return 0.5;
-        //     }
-        // })
-        // .style("stroke-opacity", function(data) {
-        //     if (data.destination == dest) {
-        //         return 1;
-        //     } else {
-        //         return 0.8;
-        //     }
-        // });
+  exports.reloadMapWithTimePeriod = function(timePeriod) {
+    var duration = 200;
+    var timePeriodFile = 'data/map/map_' + timePeriod + '.json';
+    console.log('reloadMapWithTimePeriod: ' + timePeriodFile);
+    svg.selectAll(".lca-spot")
+      .transition()
+      .duration(duration)
+      .ease("quad")
+      .style('opacity', 0);
 
+    setTimeout(function() {
+      d3.json(timePeriodFile, function(error, data) {
+        var spotSquares = svg.selectAll(".lca-spot")
+          .data(data);
+
+        spotSquares.enter().append("rect")
+          .attr("class", "lca-spot");
+
+        spotSquares
+          .attr("x", function(d) {
+            if (d.long) {
+              return x(d.long);
+            } else {
+              return 0
+            }
+          })
+          .attr("y", function(d) {
+            if (d.lat) {
+              return y(d.lat);
+            } else {
+              return 0
+            }
+            // return y(d.Latitude); 
+          })
+          .attr('width', 10)
+          .attr('height', 10)
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide)
+          .on('mousedown', function(d, i) {
+            filter('spotSelected', d, i);
+          });
+
+        spotSquares
+          .transition()
+          .duration(duration)
+          .ease("quad")
+          .style('opacity', 1);
+
+        spotSquares.exit().remove();
+      });
+    }, duration);
+  };
+
+  exports.highlightSpotsForMovie = function(movieID) {
+
+    if (movieID == null) {
+      svg.selectAll(".lca-spot")
+        .style('stroke', '#FFCD00');
+    } else {
+      svg.selectAll(".lca-spot")
+        .style('stroke', function(data) {
+          if (movieID == data.movieid) {
+            return '#FFCD00';
+          } else {
+            return '#646464';
+          }
+        });
     };
 
-    return exports;
+    // .style("stroke", function(data) {
+    //     if (data.destination == dest) {
+    //         return "#619EB5";
+    //     } else {
+    //         return "#C8C8C8";
+    //     }
+    // })
+    // .style("stroke-width", function(data) {
+    //     if (data.destination == dest) {
+    //         return 2;
+    //     } else {
+    //         return 0.5;
+    //     }
+    // })
+    // .style("stroke-opacity", function(data) {
+    //     if (data.destination == dest) {
+    //         return 1;
+    //     } else {
+    //         return 0.8;
+    //     }
+    // });
+
+  };
+
+  return exports;
 }();
 
 var filter = function(action, value, index) {
-    if (action == 'timerperiod') {
-        sm.reloadMapWithTimePeriod(value);
-        mt.reloadMovieWithTimePeriod(value);
-    };
+  if (action == 'timerperiod') {
+    sm.reloadMapWithTimePeriod(value);
+    mt.reloadMovieWithTimePeriod(value);
+  };
 
-    if (action == 'movieTableHover') {
-        sm.highlightSpotsForMovie(value.movieid);
-    };
+  if (action == 'movieTableHover') {
+    sm.highlightSpotsForMovie(value.movieid);
+  };
 
-    if (action == 'movieTableHoverEnd') {
-        sm.highlightSpotsForMovie(null);
-    };
+  if (action == 'movieTableHoverEnd') {
+    sm.highlightSpotsForMovie(null);
+  };
 
-    if (action == 'movieTableSelected') {
+  if (action == 'movieTableSelected') {
 
-    };
+  };
 
-    if (action == 'spotSelected') {
-        //show modal for movie
-        console.log(value.movieid);
-        angular.element('#movieModal').scope().updateForMovieID(value.movieid)
-        $('#movieModal').modal('show');
-    };
+  if (action == 'spotSelected') {
+    //show modal for movie
+    console.log(value.movieid);
+    angular.element('#movieModal').scope().updateForMovieID(value.movieid)
+    $('#movieModal').modal('show');
+  };
 
-    if (action == 'search') {};
+  if (action == 'search') {};
 
 };
 
