2568edbf61707737842d29718134a6f26301acc6
Shubham Goel <shubham.goel@gmail.com>
2015-05-16 23:38:52 -0700

    spot map scoped

--- js/modules/shotmap.js
+++ js/modules/shotmap.js
@@ -1,124 +1,130 @@
-var margin = {
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
-    },
-    width = 675 - margin.left - margin.right,
-    height = 675 - margin.top - margin.bottom;
+// spot map
+var sm = function() {
+    var exports = {};
+    var margin = {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        },
+        width = 675 - margin.left - margin.right,
+        height = 675 - margin.top - margin.bottom;
 
-var mapBounds = {
-    top: 37.84,
-    right: -122.35,
-    bottom: 37.68,
-    left: -122.55
-};
-// new google.maps.LatLng(37.68, -122.55),//lower box
-// new google.maps.LatLng(37.84, -122.35))
+    var mapBounds = {
+        top: 37.84,
+        right: -122.35,
+        bottom: 37.68,
+        left: -122.55
+    };
+    // new google.maps.LatLng(37.68, -122.55),//lower box
+    // new google.maps.LatLng(37.84, -122.35))
 
-var x = d3.scale.linear()
-    .range([0, width])
-    .domain([mapBounds.left, mapBounds.right]);
+    var x = d3.scale.linear()
+        .range([0, width])
+        .domain([mapBounds.left, mapBounds.right]);
 
-var y = d3.scale.linear()
-    .range([height, 0])
-    .domain([mapBounds.bottom, mapBounds.top]);
+    var y = d3.scale.linear()
+        .range([height, 0])
+        .domain([mapBounds.bottom, mapBounds.top]);
 
-var color = d3.scale.category10();
+    var color = d3.scale.category10();
 
-// var xAxis = d3.svg.axis()
-//     .scale(x)
-//     .orient("bottom");
+    // var xAxis = d3.svg.axis()
+    //     .scale(x)
+    //     .orient("bottom");
 
-// var yAxis = d3.svg.axis()
-//     .scale(y)
-//     .orient("left");
+    // var yAxis = d3.svg.axis()
+    //     .scale(y)
+    //     .orient("left");
 
-var svg = d3.select(".lca-spot-map").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select(".lca-spot-map").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("data/time_1990.json", function(error, data) {
-    // console.log(data);
-    svg.selectAll(".dot")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "dot")
-        // .attr("r", 3.5)
-        .attr("x", function(d) {
-            if (d.Longitude) {
-                return x(d.Longitude);
-            } else {
-                return 0
-            }
-        })
-        .attr("y", function(d) {
-            if (d.Latitude) {
-                return y(d.Latitude);
-            } else {
-                return 0
-            }
-            // return y(d.Latitude); 
-        })
-        .attr('width', 10)
-        .attr('height', 10);
-    // .style("fill", 'red' );
-});
+    d3.json("data/time_1990.json", function(error, data) {
+        // console.log(data);
+        svg.selectAll(".dot")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "dot")
+            // .attr("r", 3.5)
+            .attr("x", function(d) {
+                if (d.Longitude) {
+                    return x(d.Longitude);
+                } else {
+                    return 0
+                }
+            })
+            .attr("y", function(d) {
+                if (d.Latitude) {
+                    return y(d.Latitude);
+                } else {
+                    return 0
+                }
+                // return y(d.Latitude); 
+            })
+            .attr('width', 10)
+            .attr('height', 10);
+        // .style("fill", 'red' );
+    });
 
-var reloadMapWithTimePeriod = function (timePeriod) {
-    var duration = 200;
-    var timePeriodFile = 'data/map/map_' + timePeriod + '.json';
-    console.log('reloadMapWithTimePeriod: ' + timePeriodFile);
-    svg.selectAll(".dot")
-        .transition()
-        .duration(duration)
-        .ease("quad")
-        .style('opacity', 0);
+    exports.reloadMapWithTimePeriod = function(timePeriod) {
+        var duration = 200;
+        var timePeriodFile = 'data/map/map_' + timePeriod + '.json';
+        console.log('reloadMapWithTimePeriod: ' + timePeriodFile);
+        svg.selectAll(".dot")
+            .transition()
+            .duration(duration)
+            .ease("quad")
+            .style('opacity', 0);
 
-    setTimeout(function() {
-        d3.json(timePeriodFile, function(error, data) {
-            var spotSquares = svg.selectAll(".dot")
-                .data(data);
+        setTimeout(function() {
+            d3.json(timePeriodFile, function(error, data) {
+                var spotSquares = svg.selectAll(".dot")
+                    .data(data);
 
-            spotSquares.enter().append("rect")
-                .attr("class", "dot");
+                spotSquares.enter().append("rect")
+                    .attr("class", "dot");
 
-            spotSquares
-                .attr("x", function(d) {
-                    if (d.long) {
-                        return x(d.long);
-                    } else {
-                        return 0
-                    }
-                })
-                .attr("y", function(d) {
-                    if (d.lat) {
-                        return y(d.lat);
-                    } else {
-                        return 0
-                    }
-                    // return y(d.Latitude); 
-                })
-                .attr('width', 10)
-                .attr('height', 10);
+                spotSquares
+                    .attr("x", function(d) {
+                        if (d.long) {
+                            return x(d.long);
+                        } else {
+                            return 0
+                        }
+                    })
+                    .attr("y", function(d) {
+                        if (d.lat) {
+                            return y(d.lat);
+                        } else {
+                            return 0
+                        }
+                        // return y(d.Latitude); 
+                    })
+                    .attr('width', 10)
+                    .attr('height', 10);
 
-            spotSquares
-                .transition()
-                .duration(duration)
-                .ease("quad")
-                .style('opacity', 1);
+                spotSquares
+                    .transition()
+                    .duration(duration)
+                    .ease("quad")
+                    .style('opacity', 1);
 
-            spotSquares.exit().remove();
-        });
-    }, duration);
-}
+                spotSquares.exit().remove();
+            });
+        }, duration);
+    }
+
+    return exports;
+}();
 
 var filter = function(filter, value) {
         if (filter == 'timerperiod') {
             console.log('timeperiod ' + value);
-            reloadMapWithTimePeriod(value);
+            sm.reloadMapWithTimePeriod(value);
 
             mt.reloadMovieWithTimePeriod(value);
         };
