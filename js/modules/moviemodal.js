dfd0d2a5cc058f830c764a4c416b634df1b1b2a4
Shubham Goel <shubham.goel@gmail.com>
2015-05-18 02:50:25 -0700

    image posters loaded locally

--- js/modules/moviemodal.js
+++ js/modules/moviemodal.js
@@ -8,12 +8,17 @@ app.controller('MovieModalCtrl', ['$scope', '$http',
     $scope.updateForMovieID = function(movieid) {
 
       if (movieid == '') {
-        $scope.movie = {Title:'Cound Not Find Movie'};
+        $scope.movie = {
+          Title: 'Cound Not Find Movie'
+        };
       } else {
         var movieURL = 'http://www.omdbapi.com/?i=' + movieid + '&plot=full&r=json';
 
         $http.get(movieURL).
         success(function(data, status, headers, config) {
+          if (data.Poster != 'N/A') {
+            data.Poster = 'data/posters/' + data.imdbID + '.jpg';
+          }
           $scope.movie = data;
         }).
         error(function(data, status, headers, config) {
